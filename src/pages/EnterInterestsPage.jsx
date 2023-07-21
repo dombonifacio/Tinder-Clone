fdbde9918551e5ce35a38f46c52fbd882fa5d6f0
dombonifacio <nikkibonifacio184@gmail.com>
2023-07-20 19:03:17 -0700

    Fixed Selection of interest logic

--- src/pages/EnterInterestsPage.jsx
+++ src/pages/EnterInterestsPage.jsx
@@ -20,26 +20,51 @@ export const EnterInterestsPage = () => {
     const usersCollectionRef = collection(db, "users")
 
     const [interestAmount, setInterestAmount] = useState(0)
-    const [userInterest, setUserInterestList] = useState([])
 
+    const [userInterest, setUserInterestList] = useState([])
+    const [interestList, setInterestList] = useState(interestsData.interests)
     const { userInfo, setUserInfo } = useContext(UserInfoContext)
+
+    
     const handleCheckedInterest = (event) => {
         const selectedInterest = {
             name: event.target.name,
-            id: event.target.id
+            id: event.target.id,
+            isChecked: event.target.checked
         }
+        
+       
+        
+        
 
         // only allow user to check if interest amount is less than 5 or equal to 5
         if(event.target.checked && !userInterest.some((interest) => interest.id === selectedInterest.id)){
             if (interestAmount < 5) {
 
+                
+                // get the selected interest id from the interestList and see if it matches with the selectedInterest id
+                const afterChecking = interestList.map((interest) => {
+                    if (interest.name === event.target.name){
+                        interest.isChecked = !interest.isChecked
+                        // console.log(interestList, 'interest list after checking')
+                        // console.log('selected interst id ', event.target.id, 'interest list id', interest.id)
+                       
+                    }
+    
+                    return interest
+                })
+                setInterestList(afterChecking)
+               
                 setInterestAmount((prevState) => prevState + 1)
                 setUserInterestList((prevList) => [...prevList, selectedInterest])
+                 // change the JSON array to checked when event is clicked to checked
+                
 
             } else {
-                // If the user tries to select more than five interests, prevent further selection
-                event.preventDefault();
-     
+              
+              
+                // interest amount is 5 or higher, make the other interests that have isChecked set to false, be disabled
+                
             }
                 
             
@@ -48,19 +73,24 @@ export const EnterInterestsPage = () => {
             setUserInterestList((prevList) => prevList.filter((interest) => interest.id !== selectedInterest.id));
             setInterestAmount((prevState) => prevState - 1)
 
+            // interest amount is 5 or higher, make the other interests that have isChecked set to false, be disabled
+            // get the selected interest id from the interestList and see if it matches with the selectedInterest id
+            const afterChecking = interestList.map((interest) => {
+                if (interest.name === event.target.name){
+                    interest.isChecked = !interest.isChecked
+                    // console.log(interestList, 'interest list after checking')
+                    // console.log('selected interst id ', event.target.id, 'interest list id', interest.id)
+                    console.log(interestList, ' interest list')
+                }
+
+                return interest
+            })
+            setInterestList(afterChecking)
+
+
 
         }
-
     }
-
-    console.log(interestAmount, 'interest amount')
-
-
-    
-  
-
-    const [interestList] = useState(interestsData.interests)
-    
     
     const handleCreateUser = async () => {
         try {
@@ -75,43 +105,43 @@ export const EnterInterestsPage = () => {
         }
     }
 
-    const handleClick = () => {
-        console.log('label clicked')
-    }
 
+
+    console.log(userInterest, ' user interest list')
+   
    
     return (
         <>
-       <div className="flex h-screen px-4 items-center justify-center">
+          <div className="flex h-screen px-4 items-center justify-center">
             <div className="flex flex-col w-full gap-y-10">
-                <div className="">
-
-                    <h1 className="font-bold text-4xl mb-4">Choose up to 5 interests</h1>
-                    <ul className={`flex flex-wrap gap-y-6 gap-x-2`}>
-                        {interestList.map((interest) => (
-                        <li key={interest.id}>
-                            <input type="checkbox" id={interest.id} name={interest.name}  onChange={handleCheckedInterest}/>
-                             <label for={interest.id} className={`py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700`} onClick={handleClick}>{interest.name}
-                                
-                            </label>
-                        </li>
-                        ))}
-                    </ul>
-                </div>
-                <div>
-
-                    <button class="bg-gradient-to-t from-electric-pink  to-fiery-rose rounded-full hover:from-pink-700 hover:to-rose-500  text-white font-bold py-3 px-4  w-full" onClick={handleCreateUser}>
-                    Create User
-                    </button>
-                </div>
+              <div>
+                <h1 className="font-bold text-4xl mb-4">Choose up to 5 interests</h1>
+                <ul className={`flex flex-wrap gap-y-6 gap-x-2`}>
+                  {interestList.map((interest) => (
+                    <li key={interest.id} className="">
+                      <input
+                        type="checkbox"
+                        id={interest.id}
+                        name={interest.name}
+                        onChange={handleCheckedInterest}
+                        disabled={!interest.isChecked && interestAmount >= 5}
+                       
+                      />
+                      <label htmlFor={interest.id}>{interest.name}</label>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <button
+                  className="bg-gradient-to-t from-electric-pink to-fiery-rose rounded-full hover:from-pink-700 hover:to-rose-500 text-white font-bold py-3 px-4 w-full"
+                  onClick={handleCreateUser}
+                >
+                  Create User
+                </button>
+              </div>
             </div>
-           
-               
-        </div>
-        
-
-      
-
+          </div>
         </>
-    )
+      );
 }
